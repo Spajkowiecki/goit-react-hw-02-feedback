bf226ac817bfedcb3248b9d67090c19316d76386
Spajkowiecki <pcio10@wp.pl>
2023-03-29 18:28:31 +0200

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,60 @@
-import style from './App.module.css';
 import Section from './Section/Section';
+import { Component } from 'react';
+import style from './App.module.css';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notification/Notification';
+//klasowo
+export class App extends Component {
+  //
 
-export const App = () => {
-  return (
-    <div className={style.app}>
-      <Section title="Leave Feedback">
-        <FeedbackOptions options={''} />
-      </Section>
-      <Section title="Statistics">
-        {
-          <Statistics
-            good={1}
-            neutral={2}
-            bad={3}
-            total={0}
-            positivePercentage={30}
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    if (e.target.textContent === 'good') {
+      this.setState({ good: this.state.good + 1 });
+    }
+    if (e.target.textContent === 'neutral') {
+      this.setState({ neutral: this.state.neutral + 1 });
+    }
+    if (e.target.textContent === 'bad') {
+      this.setState({ bad: this.state.bad + 1 });
+    }
+  };
+
+  countTotalFeedback(options) {
+    const sum = Object.values(options).reduce((prev, el) => prev + el, 0);
+    return sum;
+  }
+
+  render() {
+    return (
+      <div className={style.app}>
+        <Section title="Leave Feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
           />
-        }
-      </Section>
-    </div>
-  );
-};
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback(this.state) === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,10 +1,19 @@
 import style from './FeedbackOptions.module.css';
-import '../Section/Section.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div className={style.options}>
-      <ul className={style.list}></ul>
+      <ul className={style.list}>
+        {Object.keys(options).map((option, i) => {
+          return (
+            <li key={'b' + i}>
+              <button className={style.button} onClick={onLeaveFeedback}>
+                {option}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
